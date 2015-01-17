0c88c254e77bec40be7bd7f574bed4f5caa9ee29
charredlot <charredlot@gmail.com>
2015-01-16 21:28:15 -0800

    display call answered date

---
+++ hidden_settings.py
@@ -0,0 +1,5 @@
+
+# stuff that doesn't need to go into git
+class HiddenSettings(object):
+    GAUTH_CLIENT_ID = '959104160060-5bnte4j2416us7pu91qua26gf8j9c3b4.apps.googleusercontent.com'
+    GAUTH_CLIENT_SECRET = 'anmrc4JrRPdNtqtkWVcY6izm'
