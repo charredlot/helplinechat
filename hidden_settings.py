ec8e44167812b7528832814d576e626e007151ae
charredlot <charredlot@gmail.com>
2015-01-20 21:20:27 -0800

    placeholder for recaptcha secret

--- hidden_settings.py
+++ hidden_settings.py
@@ -1,5 +1,7 @@
 
 # stuff that doesn't need to go into git
 class HiddenSettings(object):
-    GAUTH_CLIENT_ID = '959104160060-5bnte4j2416us7pu91qua26gf8j9c3b4.apps.googleusercontent.com'
-    GAUTH_CLIENT_SECRET = 'anmrc4JrRPdNtqtkWVcY6izm'
+    GAUTH_CLIENT_ID = ''
+    GAUTH_CLIENT_SECRET = ''
+    GAUTH_RECAPTCHA_SECRET = ''
+    
